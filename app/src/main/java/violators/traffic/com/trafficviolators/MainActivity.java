f34a360e8aa073e10db6b11472c86a3480ada9a6
JIBIN-P <philiposejibin@gmail.com>
2018-02-17 05:33:54 +0530

    Authentication with firebase is done now activity linking is just left

--- app/src/main/java/violators/traffic/com/trafficviolators/MainActivity.java
+++ app/src/main/java/violators/traffic/com/trafficviolators/MainActivity.java
@@ -1,13 +1,25 @@
 package violators.traffic.com.trafficviolators;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
+    Button startbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startbtn = (Button) findViewById(R.id.startbutton);
+        startbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        setContentView(R.layout.activity_signup);
     }
 }
